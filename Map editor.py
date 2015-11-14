a754e202518beb697d0266a11e0fcae58a8fc615
Gromms <gromet.spaal@gmail.com>
2015-11-14 15:49:12 +0200

    Added List-String based map defining.

--- Map editor.py
+++ Map editor.py
@@ -11,8 +11,15 @@ screen.fill((255, 255, 255))
 pygame.display.set_caption('Map Editor')
 
 rectPos = []
+mapList = []
 
 black = (0, 0, 0, 255)
+
+for i in range(0, screen_y // tile_size):
+	mapList.append([])
+	for j in range(0, screen_x // tile_size):
+		mapList[i].append('_')
+print(mapList)
 
 pygame.display.flip()
 
@@ -21,6 +28,13 @@ while True:
 	mouse_posx, mouse_posy = pygame.mouse.get_pos()
 	mouse_posx = mouse_posx // tile_size * tile_size
 	mouse_posy = mouse_posy // tile_size * tile_size
+
+	tile_center_x = mouse_posx + tile_size // 2
+	tile_center_y = mouse_posy + tile_size // 2
+
+	tile_pos_x = mouse_posx // tile_size
+	tile_pos_y = mouse_posy // tile_size
+	
 	tile = pygame.Rect(mouse_posx, mouse_posy, tile_size, tile_size)
 	#pygame.draw.rect(screen, (0, 0, 0), tile)
 
@@ -32,28 +46,21 @@ while True:
 	if event.type == pygame.QUIT:
 		break
 	elif event.type == pygame.MOUSEBUTTONDOWN:
-		rectPos.append(pygame.Rect(mouse_posx, mouse_posy, tile_size, tile_size))
+		if pygame.mouse.get_pressed()[0]:
+			rect = pygame.Rect(mouse_posx, mouse_posy, tile_size, tile_size)
+			rectPos.append(rect)
+			mapList[tile_pos_y][tile_pos_x] = 'X'
+			print(mapList)
 
-		
 	elif event.type == pygame.KEYDOWN:
-		open('map_name.txt', 'w').close()
-		print('asd')
-		#map_name = input('Map name: ')
-		#map_name += '.txt'
-		for y in range(0, int(screen_y / tile_size)):
-			y1 = int(y * tile_size + int(tile_size/2))
-			mapList = ''
-			for x in range(0, int(screen_x / tile_size)):
-				x1 = int(x * tile_size + int(tile_size/2))
-				print(x1, y1, tuple(screen.get_at((x1, y1))))
-				if tuple(screen.get_at((x1+1, y1+1))) == black:
-					print(x, y, 'X')
-					mapList += 'X'
-				else:
-					mapList += '_'
-			mapList += '\n'
-			with open('map_name.txt', 'a') as f:
-				f.write(mapList)
+
+		f = open('map_name.txt', 'w')
+		for y in range(0, len(mapList)):
+			for x in range(0, len(mapList[y])):
+				f.write(mapList[y][x])
+			f.write('\n')
+		print('Map saved!')
+			
 	pygame.display.update()
 	#print(tile)
 	
