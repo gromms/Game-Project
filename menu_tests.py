2eb77742be7bac371a1b931a43b68451d0e51cd1
G4ben <martinmaidla@gmail.com>
2015-12-14 23:30:39 +0200

    Added font and menu object files

---
+++ menu_tests.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.locals import *
+from menu_objects import *
+
+pygame.init()
+
+screen_x = 800
+screen_y = 600
+
+screen = pygame.display.set_mode((screen_x, screen_y))
+screen.fill((0,255,255))
+pygame.display.set_caption("Menu_tests")
+
+pygame.display.flip()
+
+Title_Menu = Title_Menu(screen)
+Game_Menu = Game_Menu(screen)
+
+Title_Menu_dif = False
+Game_Menu_dif = True
+running = True
+
+while running:
+
+    
+    
+
+    if Title_Menu_dif:
+        Title_Menu.draw()
+
+    if Game_Menu_dif:
+        Game_Menu.draw()
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
+            running = False
+
+    pygame.display.flip()
+
+    pygame.display.update()
+
+pygame.quit()
