7021162db49b2fcb9d68c4aa8aeb2963d8dfff43
Gromms <gromet.spaal@gmail.com>
2015-12-15 15:01:18 +0200

    Added "case" functionality to choose current state of game.

--- menu_objects.py
+++ menu_objects.py
@@ -1,10 +1,21 @@
 import pygame
+import os
+from map_editor import *
+import case_handler as ch
 
 class Title_Menu(pygame.font.Font):
 
-    def __init__(self, screen):
+    def __init__(self, screen, case):
 
         self.screen = screen
+
+        self.case = case
+
+        self.screen_x = 800
+        self.screen_y = 600
+
+        self.screen = pygame.display.set_mode((self.screen_x, self.screen_y))
+        self.screen.fill((255,255,255))
 
         self.Title_Title_Font = pygame.font.Font("menu_font.ttf", 60)
         self.Title_Menu_Font = pygame.font.Font("menu_font.ttf", 30)
@@ -12,6 +23,8 @@ class Title_Menu(pygame.font.Font):
         self.red = (255, 0 , 0)
         self.blue = (0, 0, 255)
         self.black = (0, 0, 0)
+
+
 
         self.Title_Title = self.Title_Title_Font.render("menu test", 1, self.black)
 
@@ -22,13 +35,13 @@ class Title_Menu(pygame.font.Font):
         self.Title_Menu_Items = {
         "Continue": (self.Title_Menu_Item_x, self.Title_Menu_Item_y), 
         "New Game": (self.Title_Menu_Item_x, self.Title_Menu_Item_y + self.Title_Menu_Item_y_spacer), 
-        "Level Editor": (self.Title_Menu_Item_x, self.Title_Menu_Item_y + 2* self.Title_Menu_Item_y_spacer), 
+        "Map Editor": (self.Title_Menu_Item_x, self.Title_Menu_Item_y + 2* self.Title_Menu_Item_y_spacer),
         "Quit": (self.Title_Menu_Item_x, self.Title_Menu_Item_y + 3* self.Title_Menu_Item_y_spacer)
         }
 
     def draw(self):
         self.screen.blit(self.Title_Title, (100,100))
-
+        #self.screen.fill((255,255,255))
         for Title_Menu_Item in self.Title_Menu_Items:
             self.Title_Menu = self.Title_Menu_Font.render(Title_Menu_Item, 1, self.red)
             if self.Title_Menu.get_rect(topleft = (self.Title_Menu_Items[Title_Menu_Item])).collidepoint(pygame.mouse.get_pos()):
@@ -36,13 +49,14 @@ class Title_Menu(pygame.font.Font):
                 if pygame.mouse.get_pressed()[0]:
                     if Title_Menu_Item == "Quit":
                         pygame.quit()
-                        sys.exit()
                     elif Title_Menu_Item == "Continue":
                         print("Continue")
                     elif Title_Menu_Item == "New Game":
                         print("New Game")
-                    elif Title_Menu_Item == "Level Editor":
-                        print("Level Editor")
+                    elif Title_Menu_Item == "Map Editor":
+                        self.case.update(ch.updateDict(self.case, 'Map Editor'))
+                    elif Title_Menu_Item == "Options":
+                        options_menu()
 
             self.screen.blit(self.Title_Menu, self.Title_Menu_Items[Title_Menu_Item])
 
@@ -51,9 +65,10 @@ class Title_Menu(pygame.font.Font):
         return self.rect
 
 
+
 class Game_Menu(pygame.font.Font):
 
-    def __init__(self, screen):
+    def __init__(self, screen, case):
 
         self.screen = screen
 
@@ -88,7 +103,6 @@ class Game_Menu(pygame.font.Font):
                 if pygame.mouse.get_pressed()[0]:
                     if Game_Menu_Item == " Quit":
                         pygame.quit()
-                        sys.exit()
                     elif Game_Menu_Item == " Options":
                         print("Options")
                         
