b1abdd011f4476cbfe047adc4addce88fdc88e6f
Gromms <gromet.spaal@gmail.com>
2015-12-15 14:22:26 +0200

    Case handler lib.

---
+++ case_handler.py
@@ -0,0 +1,12 @@
+def updateDict(case, obj):
+    obj = obj
+
+    newCase = {}
+    for i in case:
+        #print(i)
+        if i != obj:
+            newCase[i] = False
+        elif i == obj:
+            newCase[i] = True
+    #print(newCase)
+    return newCase
